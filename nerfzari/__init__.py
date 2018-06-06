f7452d737ba9267ba8e5ce82170afb904a005685
Alexander Groden <alexander.groden@gmail.com>
2018-06-06 08:48:25 -0400

    bring in changes

--- nerfzari/__init__.py
+++ nerfzari/__init__.py
@@ -4,6 +4,7 @@
 
 from nerfzari.network import Authenticator, SSHServer, SSHCmd
 from nerfzari.config import ConfigStore, Configurable
+from nerfzari.game import Game, User, Player
 
 __license__ = 'MIT'
 __all__ = ['network']
