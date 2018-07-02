c82a816ae605833418c46408ba2c40c3b3560510
Alexander Groden <alexander.groden@gmail.com>
2018-07-02 01:43:56 -0400

    commands mostly working now

--- nerfzari.py
+++ nerfzari.py
@@ -13,7 +13,7 @@ import time
 import random
 import peewee
 from datetime import datetime
-from nerfzari import ConfigStore, Authenticator, SSHServer, SSHCmd, Game, User, username
+from nerfzari import ConfigStore, Authenticator, SSHServer, SSHCmd, GameEngine, GameMeta, User, username
 
 
 logging.basicConfig(filename='nerfzari.log', level=logging.DEBUG)
@@ -75,23 +75,19 @@ class NerfzariCmd(SSHCmd):
 	# kill - ID of participant
 	# suicide - remove yourself from game
 	prompt = '>'
-	'''
-	def __init__(self, chan, username, completekey='tab'):
-		super().__init__(chan, username, completekey)
-		methods = inspect.getmembers(self, predicate=inspect.ismethod)
-		self._game_subcommands = [x[0].split('_')[0] for x in methods if x[0].endswith('game')]
-	'''
 
 	def preloop(self):
 		# check if this is the first time the user has logged in
 		self._user = User.get_by_username(self._username)
 		if self._user is None:
 			self.poutput('processing login, retrieving user data', end='')
+			'''
 			for _ in range(3):
 				time.sleep(0.5)
 				self.poutput('.', end='')
 			time.sleep(0.75)
 			self.poutput('')
+			'''
 			self.poutput('error: user not found - please enter your information to access the server')
 			name = self.terminput('full name>', False)
 			email = self.terminput('email>', False)
@@ -100,111 +96,150 @@ class NerfzariCmd(SSHCmd):
 		# get a random quote
 		#self.poutput(random_quote())
 		# display game info
-		curr_games = Game.active_games()
+		curr_games = GameMeta.active_games()
 		if len(curr_games) > 1:
 			self.poutput('current game is {}'.format(curr_games[0].name)) # TODO: show all current games
 		else:
-			next_game = Game.next_game()
+			next_game = GameMeta.next_game()
 			if next_game is not None:
 				self.poutput('next scheduled game is {} on {}'.format(next_game.name, next_game.start_date))
 
-	def new_game(self):
-		# TODO: finish type completer from discovered game types
-		def type_completer(text):
-			pass
-		gtype = self.terminput('game type>', False)
+	def new_game(self, line=None):
+		def type_completer(text, line, bidx, eidx):
+			# TODO: finish type completer from discovered game types
+			if text is None:
+				ret = GameMeta.game_types()[:]
+			else:
+				ret = [x for x in GameMeta.game_types() if x.startswith(text)]
+			return ret
+		gtype = self.terminput('game type>', tab_completer=type_completer)
 		name = self.terminput('game name>', False)
 		date_str = self.terminput('start date (month/day/year)>', False)
 		month, day, year = date_str.split('/')
 		date = datetime(int(year), int(month), int(day), 12, 0, 0, 0)
 		try:
-			Game.new_game(date, gtype, name, self._user)
+			GameMeta.new_game(date, gtype, name, self._user)
 		except peewee.IntegrityError:
 			self.poutput('a game with that name or date already exists.')
 
-	def list_game(self):
-		games = Game.list_games()
-		self.draw_gamelist(games)
+	def list_game(self, line=None):
+		self.draw_gamelist(GameMeta.list_games())
 
-	def delete_game(self):
-		games = Game.list_games()
-		print('games: {}'.format(games))
-		if len(games) > 0:
-			self.draw_gamelist(games)
+	def select_game(self, line=None):
+		if line is not None:
+			name = line
+		else:
+			self.draw_gamelist(GameMeta.list_games())
 			name = self.terminput('game name>', False)
-			game = Game.get(name=name, creator=self._user)
-			if game.start_date < datetime.now:
-				self.poutput('cannot delete games that are in progress')
-				return
-			players = [x for x in game.players]
-			if len(players) > 0:
-				self.poutput('players have joined this game, please confirm you wish to delete this game')
-				confirm = self.terminput('confirm>', False)
-			else:
-				confirm = 'yes'
-			if confirm.lower() in ['y', 'yes']:
-				game.safe_delete()
-		else:
-			self.poutput('you may only delete games you have created')
+		return name
 
-	def join_game(self):
-		self.draw_gamelist(Game.list_games())
-		name = self.terminput('game name>', False)
+	def join_game(self, line=None):
+		name = self.select_game(line)
 		if not self._user.has_joined(name):
-			game = Game.get(name=name)
-			if game.start_date < datetime.now:
-				self.poutput('cannot join a game currently in session')
-		player = game.join(self._user)
-		self.poutput('your game tag is {}'.format(player.tag))
-		self.poutput('your game tag represents you - remember it')
-		self.poutput('when you are eliminated, you give your tag to the player that eliminated you')
-		self.poutput('if a player discovers your tag, they can use it to eliminate you, so keep it secret!')
-
-	def leave_game(self):
-		# TODO: only allow leave if game hasn't started yet
-		self.draw_gamelist(Game.list_games())
-		name = self.terminput('game name>', False)
-		game = Game.get(name=name)
-		if not game.started():
-			Game.leave_game(self._user, Game.get(name=name))
+			try:
+				return GameMeta.get(name=name)
+			except peewee.DoesNotExist:
+				self.poutput('game {} does not exist'.format(name))
+				return None
+			if meta.active():
+				self.poutput('cannot join an active game')
+				return
+			player = meta.game.join(self._user)
+			self.poutput('your game tag is {}'.format(player.tag))
+			self.poutput('your game tag represents you - remember it')
+			self.poutput('when you are eliminated, you give your tag to the player that eliminated you')
+			self.poutput('if a player discovers your tag, they can use it to eliminate you, so keep it secret!')
 		else:
-			self.poutput('cannot leave a game that has started')
+			self.poutput('you have already joined this game')
 
-	def show_tag(self):
-		game = Game.next_game()
-		if game:
-			self.poutput('your current tag is: {}'.format(game.tag(self._user)))
+	def leave_game(self, line=None):
+		name = self.select_game(line)
+		if self._user.has_joined(name):
+			try:
+				meta = GameMeta.get(name=name)
+			except peewee.DoesNotExist:
+				self.poutput('game {} does not exist'.format(name))
+				return
+			if meta.active():
+				self.poutput('cannot leave an active game')
+				return
+			meta.game.leave(self._user)
 
-	def reset_tag(self):
-		self.show_tag()
-		game = Game.next_game()
-		new_tag = game.tag
-		resp = self.terminput('keep?>')
-		while resp.lower() not in ['y', 'yes']:
-			new_tag = username.generate_username(8, 8)
-			self.poutput('new tag: {}'.format(new_tag))
+	def delete_game(self, line=None):
+		name = self.select_game(line)
+		try:
+			meta = GameMeta.get(name=name)
+		except peewee.DoesNotExist:
+			self.poutput('game {} does not exist'.format(name))
+			return
+		if meta.creator != self._user:
+			self.poutput('you may only delete games you have created')
+			return
+		if meta.active():
+			self.poutput('cannot delete an active game')
+			return
+		players = [x for x in meta.players]
+		if len(players) > 0:
+			self.poutput('game has players, please confirm deletion')
+			confirm = self.terminput('confirm>', False)
+		else:
+			confirm = 'yes'
+		if confirm.lower() in ['y', 'yes']:
+			meta.safe_delete()
+
+	def tag_game(self, line=None):
+		meta = GameMeta.next_game()
+		if meta:
+			self.poutput('your tag for the game {} is: {}'.format(
+				meta.name, meta.game.tag(self._user)))
+
+	def reset_tag_game(self, line=None):
+		if line is not None:
+			name = line
+			try:
+				meta = GameMeta.get(name=name)
+			except peewee.DoesNotExist:
+				self.poutput('game {} does not exist'.format(name))
+				return
+		else:
+			meta = GameMeta.next_game()
+		if meta:
+			tag = meta.game.tag(self._user)
+			self.poutput('your tag for the game {} is: {}'.format(meta.name, tag))
+			new_tag = tag
 			resp = self.terminput('keep?>')
-		game.tag = new_tag
-		game.save()
+			while resp.lower() not in ['y', 'yes']:
+				new_tag = username.generate_username(8, 8)
+				self.poutput('new tag: {}'.format(new_tag))
+				resp = self.terminput('keep?>')
+			meta.game.reset_tag(self._user, new_tag)
 
-	_game_subcommands = ['new', 'list', 'delete', 'join', 'joined', 'leave', 'tag', 'reset_tag']
+	def start_game(self, line=None):
+		name = self.select_game(line)
+		meta = GameMeta.get(name=name)
+		if meta is not None:
+			meta.game.start()
+
+	_game_subcommands = ['new', 'list', 'delete', 'join', 'joined', 'leave', 'tag', 'reset_tag', 'start']
 	def do_game(self, line):
 		"""
 		interact with games. Has the following sub commands:
-		new - create a new game
-		delete - delete a game you created before it has started
-		list - list upcoming games
-		join - join a game
-		leave - leave a game
-		tag - show your current / next tag
+		  new - create a new game
+		  delete - delete a game you created before it has started
+		  list - list upcoming games
+		  join - join a game
+		  leave - leave a game
+		  tag - show your current / next tag
 		"""
-		if line == '':
-			line = 'list'
-		if line not in self._game_subcommands:
-			self.poutput('unrecognized subcommand: {}'.format(line))
+		parts = line.split()
+		sub_cmd = parts[0]
+		if sub_cmd == '':
+			sub_cmd = 'list'
+		if sub_cmd not in self._game_subcommands:
+			self.poutput('unrecognized subcommand: {}'.format(sub_cmd))
 			return
-		func = getattr(self, '{}_game'.format(line))
-		return func()
+		func = getattr(self, '{}_game'.format(sub_cmd))
+		return func(' '.join(parts[1:]) if len(parts) > 1 else None)
 
 	def complete_game(self, text, line, bidx, eidx):
 		if text is None:
@@ -213,15 +248,29 @@ class NerfzariCmd(SSHCmd):
 			ret = [x for x in self._game_subcommands if x.startswith(text)]
 		return ret
 
+	def get_game(self, line=None):
+		if line is not None:
+			name = line
+			try:
+				return GameMeta.get(name=name)
+			except peewee.DoesNotExist:
+				self.poutput('game {} does not exist'.format(name))
+				return
+		else:
+			return GameMeta.next_game()
+
 	def do_target(self, line):
 		"""show your current target"""
-		game = Game.next_game(self, line)
-		pass
-
+		meta = self.get_game(line)
+		if meta is not None:
+			target = meta.game.target(self._user)
+			self.poutput('your target is: {}'.format(target.real_name))
+		
 	def do_eliminate(self, line):
-		"""Eliminate a target"""
-		# TODO: process line for game tag
-		pass
+		"""eliminate a target by their game tag"""
+		meta = self.get_game(line)
+		if meta is not None:
+			meta.eliminate_by_tag(self._user, line)
 
 	def do_stats(self, line):
 		"""show your stats"""
@@ -234,17 +283,21 @@ class NerfzariCmd(SSHCmd):
 	def draw_gamelist(self, games):
 		if len(games) > 0:
 			joined = [x for x in self._user.games_joined()]
-			print('joined: {}'.format(joined))
-			header = ['start date', 'name', 'game type', 'creator', 'joined', 'winner']
+			header = ['start date', 'name', 'game type', 'creator', 'joined', 'status']
 			data = []
 			for game in games:
+				status = 'pending'
+				if game.over:
+					status = 'over'
+				if game.start_date >= datetime.now():
+					status = 'active'
 				data.append((
 					game.start_date.strftime('%M/%d/%Y'),
 					game.name,
-					game.game_type,
+					game.type,
 					game.creator.user_name,
 					'joined' if game.primary_key in joined else '',
-					game.winner
+					status
 				))
 			self.draw_table(header, data)
 
@@ -256,6 +309,9 @@ class NerfzariCmd(SSHCmd):
 			if len(row) > 0:
 				self.poutput(row)
 
+	def emptyline(self):
+		pass
+
 if __name__ == '__main__':
 	# TODO: make the game management thread
 	ConfigStore.load_all()
@@ -263,8 +319,11 @@ if __name__ == '__main__':
 	addr = ('', cfg['listen_port'])
 	key_path = cfg['rsa_key']
 	auth_cls = Authenticator.get(cfg['auth_cls'])
+	engine = GameEngine()
+	engine.start()
 	try:
 		with SSHServer(addr, key_path, NerfzariCmd, auth_cls) as server:
 			server.serve_forever()
 	except KeyboardInterrupt:
+		engine.stop()
 		server.shutdown()
